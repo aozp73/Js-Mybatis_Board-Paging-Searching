88140f5293bb5d24ecfeea4107a0d6956be0541a
aozp73 <aozp73@naver.com>
2023-12-26 14:34:38 +0900

    initial commit

---
+++ src/main/java/com/example/jsmybatis_pagingsearching/JsMybatisPagingSearchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsmybatis_pagingsearching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsMybatisPagingSearchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsMybatisPagingSearchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jsmybatis_pagingsearching/JsMybatisPagingSearchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsmybatis_pagingsearching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsMybatisPagingSearchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
