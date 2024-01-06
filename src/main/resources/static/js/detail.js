a4e58fbae369679a5ba38fb1f9fdc238c55034a1
aozp73 <aozp73@naver.com>
2024-01-06 17:31:34 +0900

    댓글 - js 함수 주석 추가

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -1,3 +1,7 @@
+/**
+ * 게시글 삭제
+ * - 이동: 게시글 목록
+ */
 function deleteBoard(boardId) {
     let confirmation = confirm("게시글을 삭제하시겠습니까?");
 
@@ -21,6 +25,11 @@ function deleteBoard(boardId) {
     }
 }
 
+/**
+ * 댓글 작성
+ * - 성공: DB 댓글 리렌더링
+ * - 실패: alert 안내
+ */
 function postComment(boardId) {
     let content = document.getElementById("commentContent").value;
     let data = {
@@ -47,6 +56,133 @@ function postComment(boardId) {
     });
 }
 
+/**
+ * 댓글 삭제
+ * - 성공: DB 댓글 리렌더링
+ * - 실패: alert 안내
+ */
+function deleteComment(commentId, boardId) {
+    let confirmation = confirm("댓글을 삭제하시겠습니까?");
+
+    if (confirmation) {
+        $.ajax({
+            url: '/auth/comment/' + boardId + '/' + commentId,
+            type: 'DELETE',
+
+            success: function(response) {
+                console.log(response)
+                appendComments(response.data, boardId);
+            },
+            error: function(error) {
+                console.log(error);
+                alert(error.responseJSON.data);
+            }
+        });
+    } else {
+    }
+}
+
+/**
+ * 댓글 수정 Form
+ * - div 출력 Form -> input 입력 Form
+ * - 수정, 삭제 -> 수정하기, 취소
+ */
+function updateFormat(commentId, boardId) {
+    let commentUpdateButtonBox = $('#commentUpdateButtonBox-' + commentId);
+    let commentContentBox = $('#commentContentBox-' + commentId);
+    let commentContent = $('#commentContent-' + commentId);
+
+    let editButton = $('<span class="custom-comment-font me-2">수정하기</span>')
+        .click(function() {
+            updateComment(commentId, boardId);
+        });
+    let cancelButton = $('<span class="custom-comment-font">취소</span>')
+        .click(function() {
+            cancelEdit(commentId, boardId);
+        });
+
+    commentUpdateButtonBox.empty().append(editButton).append(cancelButton);
+
+    let currentContent = commentContent.text();
+    let inputElement = $('<input>', {
+        id: 'commentContent-' + commentId,
+        type: 'text',
+        class: 'form-control form-control-sm',
+        value: currentContent
+    });
+
+    commentContentBox.empty().append(inputElement);
+}
+
+/**
+ * 수정하기 Form 내 기능
+ * - input 입력 Form -> div 출력 Form
+ * - 수정하기, 취소 -> 수정, 삭제
+ */
+function cancelEdit(commentId, boardId) {
+    let commentUpdateButtonBox = $('#commentUpdateButtonBox-' + commentId);
+    let commentContentBox = $('#commentContentBox-' + commentId);
+    let commentContent = $('#commentContent-' + commentId);
+
+    // 수정, 삭제 요소 추가
+    let editButton = $('<span class="custom-comment-font me-1">수정</span>')
+        .click(function() {
+            updateFormat(commentId, boardId);
+        });
+    let deleteButton = $('<span class="custom-comment-font">삭제</span>')
+        .click(function() {
+            deleteComment(commentId, boardId);
+        });
+    commentUpdateButtonBox.empty().append(editButton).append(deleteButton);
+
+    // input -> div 변경 (내용 포함)
+    let currentContent = commentContent.val();
+    let inputElement = $('<div>', {
+        id: 'commentContent-' + commentId,
+        type: 'text',
+        style: 'font-size: 14px',
+        text: currentContent
+    });
+    commentContentBox.empty().append(inputElement);
+}
+
+/**
+ * 수정하기 Form 내 기능
+ * - Server 통신 및 수정 진행
+ * - 성공 시, DB 데이터로 전체 댓글 리렌더링
+ */
+function updateComment(commentId, boardId) {
+    let content = $('#commentContent-'+ commentId).val();
+    let data = {
+        content: content,
+        boardId: boardId,
+        commentId: commentId
+    }
+
+    $.ajax({
+        url: '/auth/comment/' + boardId + '/' + commentId,
+        type: 'PUT',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(data),
+
+        success: function(response) {
+            console.log(response)
+            appendComments(response.data, boardId);
+        },
+        error: function(error) {
+            console.log(error);
+            alert(error.responseJSON.data);
+            location.href = '/board/' + boardId;
+        }
+    });
+}
+
+/**
+ * 함수
+ * - 호출 시점: 댓글 작성 / 댓글 수정 / 댓글 삭제 성공 시
+ * - 기능: 해당 게시글 전체 댓글 리렌더링
+ * - 목적: UX, 다른 사용자 추가 댓글 실시간 제공
+ */
 function appendComments(commentList, boardId) {
     let commentSection = $('#commentSection');
     commentSection.empty();
@@ -95,103 +231,4 @@ function appendComments(commentList, boardId) {
     });
 
     $('#countComment').text('댓글 ' + commentList.length + '개');
-}
-
-function deleteComment(commentId, boardId) {
-    let confirmation = confirm("댓글을 삭제하시겠습니까?");
-
-    if (confirmation) {
-        $.ajax({
-            url: '/auth/comment/' + boardId + '/' + commentId,
-            type: 'DELETE',
-
-            success: function(response) {
-                console.log(response)
-                appendComments(response.data, boardId);
-            },
-            error: function(error) {
-                console.log(error);
-                alert(error.responseJSON.data);
-            }
-        });
-    } else {
-    }
-}
-
-function updateFormat(commentId, boardId) {
-    let commentUpdateButtonBox = $('#commentUpdateButtonBox-' + commentId);
-    let commentContentBox = $('#commentContentBox-' + commentId);
-    let commentContent = $('#commentContent-' + commentId);
-
-    let editButton = $('<span class="custom-comment-font me-2">수정하기</span>')
-        .click(function() {
-            updateComment(commentId, boardId);
-        });
-    let cancelButton = $('<span class="custom-comment-font">취소</span>')
-        .click(function() {
-            cancelEdit(commentId, boardId);
-        });
-
-    commentUpdateButtonBox.empty().append(editButton).append(cancelButton);
-
-    let currentContent = commentContent.text();
-    let inputElement = $('<input>', {
-        id: 'commentContent-' + commentId,
-        type: 'text',
-        class: 'form-control form-control-sm',
-        value: currentContent
-    });
-
-    commentContentBox.empty().append(inputElement);
-}
-
-function cancelEdit(commentId, boardId) {
-    let commentUpdateButtonBox = $('#commentUpdateButtonBox-' + commentId);
-    let commentContentBox = $('#commentContentBox-' + commentId);
-    let commentContent = $('#commentContent-' + commentId);
-
-    let editButton = $('<span class="custom-comment-font me-1">수정</span>')
-        .click(function() {
-            updateFormat(commentId, boardId);
-        });
-    let deleteButton = $('<span class="custom-comment-font">삭제</span>')
-        .click(function() {
-            deleteComment(commentId, boardId);
-        });
-    commentUpdateButtonBox.empty().append(editButton).append(deleteButton);
-
-    let currentContent = commentContent.val();
-    let inputElement = $('<div>', {
-        id: 'commentContent-' + commentId,
-        type: 'text',
-        style: 'font-size: 14px',
-        text: currentContent
-    });
-    commentContentBox.empty().append(inputElement);
-}
-
-function updateComment(commentId, boardId) {
-    let content = $('#commentContent-'+ commentId).val();
-    let data = {
-        content: content,
-        boardId: boardId,
-        commentId: commentId
-    }
-
-    $.ajax({
-        url: '/auth/comment/' + boardId + '/' + commentId,
-        type: 'PUT',
-        contentType: 'application/json; charset=utf-8',
-        data: JSON.stringify(data),
-
-        success: function(response) {
-            console.log(response)
-            appendComments(response.data, boardId);
-        },
-        error: function(error) {
-            console.log(error);
-            alert(error.responseJSON.data);
-            location.href = '/board/' + boardId;
-        }
-    });
 }
